244e94f73c3c4c62e2ad77910cc718bf05df2a15
groats23 <121118901+groats23@users.noreply.github.com>
2023-05-29 09:04:24 +0200

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,27 +1,76 @@
-// https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_1SLi0nwDKLs9tE67PgHRv7wAvhwYEp2lyKt4wusq54NRJSTrg81S8J9zXoQW5oKl
-
-const URL =
-  'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_1SLi0nwDKLs9tE67PgHRv7wAvhwYEp2lyKt4wusq54NRJSTrg81S8J9zXoQW5oKl';
+// В змінну задаємо ключ API який получили при реєстрації
 const API_KEY =
   'live_1SLi0nwDKLs9tE67PgHRv7wAvhwYEp2lyKt4wusq54NRJSTrg81S8J9zXoQW5oKl';
 
-function onSearchKatze(query) {
-  return fetch(`${URL}?apiKey=${API_KEY}&katze=${query}`).then(res =>
-    res.json()  
+// Робимо HTTP запит до ресурсу
+export function fetchBreeds() {
+  const URL = 'https://api.thecatapi.com/v1/breeds';
+
+  // Отримуємо список порід котів з значенням нашого ключа
+  return (
+    fetch(URL, {
+      headers: {
+        'x-api-key': API_KEY,
+      },
+    })
+      //Після запиту повертається проміс с сервера. Обробляємо відповідь та перетворюємо в json формат
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch breeds');
+        }
+        return response.json();
+      })
+      // Перебераємо та витягуємо з оюєктів що нам потрібно
+      .then(data => {
+        return data.map(breed => ({
+          id: breed.id,
+          name: breed.name,
+        }));
+      })
   );
 }
 
-export default { onSearchKatze };
+// Робимо HTTP запит до ресурсу з значенням breedId щоб отримати інфу про кота
+export function fetchCatByBreed(breedId) {
+  const URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
 
-const BASE_URL = 'https://restcountries.com/v3.1/name';
-const PARAMS = 'name,capital,population,flags,languages';
-
-export default function fetchCountries(name) {
-  return fetch(`${BASE_URL}/${name}?fields=${PARAMS}`).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-                throw new Error(response.status);
-  });
+  // Получаємо список з значенням нашого ключа
+  return (
+    fetch(URL, {
+      headers: {
+        'x-api-key': API_KEY,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch cat');
+        }
+        return response.json();
+      })
+      // Обробляємо та повертаємо тіки першийобєкт з масиву
+      .then(data => {
+        return data[0];
+      })
+  );
 }
-´´ß`?`
+
+/**
+  |==================================
+
+   // // https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_1SLi0nwDKLs9tE67PgHRv7wAvhwYEp2lyKt4wusq54NRJSTrg81S8J9zXoQW5oKl
+
+// const URL =
+//   'https://api.thecatapi.com/v1/breeds';
+// const API_KEY =
+//   'live_1SLi0nwDKLs9tE67PgHRv7wAvhwYEp2lyKt4wusq54NRJSTrg81S8J9zXoQW5oKl';
+
+// function fetchBreeds(query) {
+//   return fetch(`${URL}?apiKey=${API_KEY}&katze=${query}`).then(res =>
+//     res.json()
+//   );
+// }
+
+// export default { fetchBreeds };
+
+  |==================================
+*/
--- src/index.js
+++ src/index.js
@@ -1,13 +1,100 @@
-import API from './cat-api.js';
+// Робимо імпорти
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import './styles.css';
 
-API.onSearchKatze('katze-all').then(result => console.log(result));
+// Звертаємось до елементів html
+const selectElement = document.getElementById('selectElement');
+const loaderElement = document.querySelector('.loader');
+const errorElement = document.querySelector('.error');
+const catInfoElement = document.querySelector('.cat-info');
 
-const refs = {
-  breedSelect: document.querySelector('#selectElement'),
-  catsWrapper: document.querySelector('.cat-info'),
-  loader: document.querySelector('.loader'),
-  error: document.querySelector('.error'),
-};
+// Отримаємо списоккотів
+fetchBreeds()
+  // Приймаємо список з параметром breeds. Створюємо новіелементи
+  .then(breeds => {
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      selectElement.appendChild(option);
+    });
 
-refs.error.classList.add('is-hidden');
-showLoader(refs.breedSelect);
+    // Робимо слухача подій для вибора породи кота
+    selectElement.addEventListener('change', () => {
+      const breedId = selectElement.value;
+      showLoader();
+      hideError();
+      // Запит для полученняінфи про кота
+      fetchCatByBreed(breedId)
+        .then(cat => displayCatInfo(cat))
+        .catch(error => displayError());
+    });
+
+    // Приховуємо завантажувач після завершення отримання списку котів
+    hideLoader();
+  })
+  .catch(error => {
+    displayError();
+    hideLoader(); // Приховуємо завантажувач у випадку помилки
+  });
+
+// Робимо видимим елемент на сторінці
+function showLoader() {
+  loaderElement.style.display = 'block';
+}
+
+// Робимо нивидимим елемент на сторінці
+function hideLoader() {
+  loaderElement.style.display = 'none';
+}
+
+// Робимо видимою помилку на сторінці
+function displayError() {
+  errorElement.style.display = 'block';
+}
+
+// Робимо нивидимою помилку на сторінці
+function hideError() {
+  errorElement.style.display = 'none';
+}
+
+// Робимо відображення інфи в елементі catInfoElement та вказуємо в елементи html
+// Побачимо зображення кота, назву породи, опис та темперамент
+function displayCatInfo(cat) {
+  loaderElement.innerHTML = '';
+  catInfoElement.innerHTML = `
+    <img src="${cat.url}" alt="Cat Image">
+    <h3>${cat.breeds[0].name}</h3>
+    <p>${cat.breeds[0].description}</p>
+    <p>Temperament: ${cat.breeds[0].temperament}</p>
+  `;
+  catInfoElement.style.display = 'block';
+}
+
+// function hideCatInfo() {
+//   catInfoElement.innerHTML = '';
+//   catInfoElement.style.display = 'none';
+// }
+
+// // Спочатку приховуємо інформацію про завантажувач, помилку і cat
+// hideLoader();
+// hideError();
+// hideCatInfo();
+
+/**
+  |==================================
+
+   // import API from './cat-api.js';
+
+// API.onSearchKatze('katze-all').then(result => console.log(result));
+
+// const refs = {
+//   breedSelect: document.querySelector('#selectElement'),
+//   catsWrapper: document.querySelector('.cat-info'),
+//   loader: document.querySelector('.loader'),
+//   error: document.querySelector('.error'),
+// };
+
+
+  |==================================
+*/
