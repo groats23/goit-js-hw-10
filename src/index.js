f2d7949e6d34b29cb2f1889814e5ed4a3939a3ae
groats23 <121118901+groats23@users.noreply.github.com>
2023-05-29 11:09:34 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-// Робимо імпорти
+// Робимо імпорти.
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import './styles.css';
 import Notiflix from 'notiflix';
 
-// Звертаємось до елементів html
+// Звертаємось до елементів html.
 const selectElement = document.getElementById('selectElement');
 const loaderElement = document.querySelector('.loader');
 const errorElement = document.querySelector('.error');
@@ -11,7 +11,7 @@ const catInfoElement = document.querySelector('.cat-info');
 
 // Отримаємо списоккотів
 fetchBreeds()
-  // Приймаємо список з параметром breeds. Створюємо новіелементи
+  // Приймаємо список з параметром breeds. Створюємо новіелементи.
   .then(breeds => {
     breeds.forEach(breed => {
       const option = document.createElement('option');
@@ -20,47 +20,47 @@ fetchBreeds()
       selectElement.appendChild(option);
     });
 
-    // Робимо слухача подій для вибора породи кота
+    // Робимо слухача подій для вибора породи кота.
     selectElement.addEventListener('change', () => {
       const breedId = selectElement.value;
       showLoader();
       hideError();
-      // Запит для полученняінфи про кота
+      // Запит для полученняінфи про кота.
       fetchCatByBreed(breedId)
         .then(cat => displayCatInfo(cat))
         .catch(error => displayError());
     });
 
-    // Приховуємо завантажувач після завершення отримання списку котів
+    // Приховуємо завантажувач після завершення отримання списку котів.
     hideLoader();
   })
   .catch(error => {
     displayError();
-    hideLoader(); // Приховуємо завантажувач у випадку помилки
+    hideLoader(); // Приховуємо завантажувач у випадку помилки.
   });
 
-// Робимо видимим елемент на сторінці
+// Робимо видимим елемент на сторінці.
 function showLoader() {
   loaderElement.style.display = 'block';
 }
 
-// Робимо нивидимим елемент на сторінці
+// Робимо нивидимим елемент на сторінці.
 function hideLoader() {
   loaderElement.style.display = 'none';
 }
 
-// Робимо видимою помилку на сторінці
+// Робимо видимою помилку на сторінці.
 function displayError() {
   Notiflix.Notify.failure('Сталася помилка. Будь ласка, спробуйте ще раз.');
 }
 
-// Робимо нивидимою помилку на сторінці
+// Робимо нивидимою помилку на сторінці.
 function hideError() {
   Notiflix.Notify.remove();
 }
 
-// Робимо відображення інфи в елементі catInfoElement та вказуємо в елементи html
-// Побачимо зображення кота, назву породи, опис та темперамент
+// Робимо відображення інфи в елементі catInfoElement та вказуємо в елементи html.
+// Побачимо зображення кота, назву породи, опис та темперамент.
 function displayCatInfo(cat) {
   loaderElement.innerHTML = '';
   catInfoElement.innerHTML = `
